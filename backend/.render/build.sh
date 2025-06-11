9398550087b1f8ffe04e0cba91b857427196b841
Avishkar <avishkarclg32@gmail.com>
2025-06-12 00:22:00 +0530

    Minor Bug fix

--- backend/.render/build.sh
+++ backend/.render/build.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+echo "🛠 Starting custom build..."
+
 apt-get update && apt-get install -y python3 python3-pip
 
 ln -sf /usr/bin/python3 /usr/bin/python
 
-# Install Python packages from backend/model/requirements.txt
+echo "📦 Installing Python requirements..."
 pip install -r model/requirements.txt
+
+echo "✅ Python environment ready"
